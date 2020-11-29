c71591bf95624276d6ef1ed8676560a2be5dec80
no0ro <nrolwes@gmail.com>
2020-11-29 09:41:51 -0500

    commit tree ec2b1f7367d734c07c8f95e3c799fbc6350996f9

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,26 @@
-// your ImageSlider code here!
+import React from 'react';
+// use controlled
+
+
+class ImageSlider extends React.Component {
+    constructor() {
+        super()
+        // set initial state
+        this.state = {
+            currentSlideIndex: 0
+        }
+    }
+
+    // 'I am on slide <CURRENT_SLIDE>', where <CURRENT_SLIDE> is the value of this.state.currentSlideIndex
+
+    render() {
+        
+        return(
+            <div>
+                I am on slide {this.state.currentSlideIndex}
+            </div>
+        )
+    }
+}
+
+export default ImageSlider
